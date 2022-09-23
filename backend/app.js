5f5267d428bbe11e8a30054b1a8c23e77160a008
Andressa <andressa.ro26@gmail.com>
2022-09-23 00:05:25 -0300

    finish

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors")
 const UsuarioRoutes = require("./src/routes/v1/UsuarioRouter");
 const CotacaoRoutes = require("./src/routes/v1/CotacaoRouter");
 const AutenticacaoRoutes = require("./src/routes/v1/AutenticacaoRouter");
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/api/v1/', UsuarioRoutes)
 app.use('/api/v1/', CotacaoRoutes)
